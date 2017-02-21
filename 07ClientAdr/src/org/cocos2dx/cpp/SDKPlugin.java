e6a7838a3b395d4f6c33788bb99fab7f7d5c990c
caoguoping <350232751@qq.com>
2017-02-21 18:50:49 +0800

    hello

--- 07ClientAdr/src/org/cocos2dx/cpp/SDKPlugin.java
+++
@@ -1,7 +0,0 @@
-package org.cocos2dx.cpp;
-
-public class SDKPlugin{
-	
-	static native void LoginCallback(String uid, String access_token);
-	static native void paySecessCallback(String orderNum, String save);   //充值成功回调
-}
